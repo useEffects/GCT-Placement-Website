7e3004e86acf172d5e5d41fd324efeeed18fb394
Joel Samuel <joel.sr1024@gmail.com>
2022-11-05 00:15:10 +0530

    Fix mui theme bug

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { createTheme, ThemeProvider } from '@mui/material'
-const {myColors} = require("../../themeOveride.js")
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+const myColors = require("../../themeOveride.js")
 const theme = createTheme({
   palette: myColors
 })
