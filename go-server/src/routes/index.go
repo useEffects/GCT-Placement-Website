974e7c922d5a980f5562fd2835408f159e7d6096
Joel Samuel <joel.sr1024@gmail.com>
2022-11-05 11:10:41 +0530

    Project reboot

--- go-server/src/routes/index.go
+++ go-server/src/routes/index.go
@@ -7,6 +7,9 @@ import (
 
 func SetupRoutes (app *fiber.App) {
     
+    app.Get("/", func(c *fiber.Ctx) error {
+	return c.SendString("Hello World!")
+    })
     api := app.Group("/api", logger.New())
     auth := api.Group("/auth")
     
