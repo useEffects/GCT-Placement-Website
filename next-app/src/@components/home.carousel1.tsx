42ecca84adbe38ef0b717473d5e8460c8bf228a1
chanlee005 <82578677+chanlee005@users.noreply.github.com>
2022-11-07 03:01:49 +0530

    added images and made some styling

--- next-app/src/@components/home.carousel1.tsx
+++ next-app/src/@components/home.carousel1.tsx
@@ -1,30 +1,34 @@
-import firstimage from "../images/logo22.png"
+import FirstImage from "../images/CompanyAmico.png"
+import SecondImage from "../images/Cheer2.png"
+import ThirdImage from "../images/MoneyIncomeAmico.png"
+
+
 export const Carousel1 = () => {
     const datas = [
         {
-            imgSrc: firstimage,
+            imgSrc: FirstImage.src,
+            header: "Number Of Companies",
+            content: "100 companies, An extensive amount of trustworthy and booming companies that hire in our college.",
+            stat: 100
+        },
+        {
+            imgSrc: SecondImage.src,
             header: "Number Of Students Placed",
             content: "300 Bright and Astute minds who have managed to get placed successfully",
             stat: 300
         },
         {
-            imgSrc: firstimage,
-            header: "Number Of Students Placed",
-            content: "300 Bright and Astute minds who have managed to get placed successfully",
-            stat: 300
-        },
-        {
-            imgSrc: firstimage,
-            header: "Number Of Students Placed",
-            content: "300 Bright and Astute minds who have managed to get placed successfully",
-            stat: 300
+            imgSrc: ThirdImage.src,
+            header: "Highest Package",
+            content: "14 LPA, A whopping amount for the deserving ones",
+            stat: 14+'LPA'
         }
 
     ]
-    return (<div className="w-full overflow-scroll bg-gray-100 h-[600px] my-4">
-        <div className={`w-[1000px] py-12 flex h-full`}>
+    return (<div className="w-full overflow-scroll bg-transparent h-[600px] my-4 lg:overflow-hidden">
+        <div className={`w-[1000px] py-12 flex h-full  lg:w-[1154px]`}>
             {datas.map((data, i) => (<div key={i} className={`shadow-lg w-1/${datas.length} bg-white rounded-3xl flex flex-col justify-around items-center h-full mx-4 py-12 px-4`}>
-                <img className="object-contain h-1/2 bg-gray-100" src={data.imgSrc.src}/>
+                <img className="object-contain h-1/2 bg-gray-100" src={data.imgSrc}/>
                 <p className="font-bold text-lg text-center"> {data.header} </p>
                 <p className="text-6xl z-[1] relative font-extrabold before:absolute before:top-6 before:-bottom-0 before:-left-2 before:-right-2 before:bg-primary-light before:z-[-1] before:rounded-tl-[9999px] before:rounded-br-[9999px]"> {data.stat} </p>
                 <p className="text-center"> {data.content} </p>
--- next-app/src/@components/home.carousel4.tsx
+++ next-app/src/@components/home.carousel4.tsx
@@ -1,24 +1,26 @@
 import { Button } from "@mui/material"
-
+import Civil from "../images/CivilHat.png"
+import Elec from "../images/Electronics.png"
+import Rect from "../images/Rectangle.png"
 export default function Carousel4 () {
     const datas = [
         {
             header: "Information Technology",
-            imgSrc: ""
+            imgSrc: Civil.src
         },
         {
             header: "Mechanincal Engineering",
-            imgSrc: ""
+            imgSrc: Elec.src
         },
         {
             header: "Computer Science and Engineering",
-            imgSrc: ""
+            imgSrc: Rect.src
         }
     ]
-     return (<div className="my-12 pb-12 w-full overflow-scroll">
-        <div className="w-[1000px] h-full flex px-4"> 
+     return (<div className="my-12 pb-12 w-full overflow-scroll lg:overflow-hidden">
+        <div className="w-[1000px] h-full flex px-4 lg:w-[1154px] items-center"> 
             {datas.map((data, i) => <div key={i} className="flex flex-col justify-evenly items-center h-full w-full mx-4 shadow-lg py-12">
-                <img className="object-contain rw-2 mx-4 h-[300px] bg-gray-300"/>
+                <img src={data.imgSrc} className="object-contain rw-2 mx-4 h-[300px] bg-gray-300"/>
                 <p className="text-center text-lg mx-4 my-12 uppercase font-bold"> {data.header} </p>
                 <Button variant="contained" className="bg-primary-main w-[150px]"> read more </Button>
             </div>)}
