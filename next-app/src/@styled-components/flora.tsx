46cdd0ceb046f603664b50e24112534b0e32ada1
Joel Samuel <joel.sr1024@gmail.com>
2022-11-06 18:11:59 +0530

    Fix: Flora bug

--- next-app/src/@styled-components/flora.tsx
+++ next-app/src/@styled-components/flora.tsx
@@ -2,7 +2,9 @@ import { ReactNode } from "react";
 
 export default function Flora ({flower = [""], className = "", children}: {flower?: string[],className?: string, children: ReactNode}) {
     return (<div className={`${className} relative overflow-clip`}>
-        <img className={`absolute object-contain ${flower.join(" ")}`} src="Frame 122.png"/>
+        {flower.map(f => 
+            <img className={`absolute object-contain ${f}`} src="Frame 122.png"/>
+        )}
         {children}
     </div>)
 }
