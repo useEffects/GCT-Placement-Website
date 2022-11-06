e48da2c3bc0fb3f4c1586741c23be72cd29c1067
Joel Samuel <joel.sr1024@gmail.com>
2022-11-06 20:48:21 +0530

    images bug

--- next-app/src/@components/home.carousel1.tsx
+++ next-app/src/@components/home.carousel1.tsx
@@ -1,22 +1,20 @@
-
-import reactimage from "../images/Happystudent-pana1.png"
 import firstimage from "../images/logo22.png"
 export const Carousel1 = () => {
     const datas = [
         {
-            imgSrc: {firstimage},
+            imgSrc: firstimage,
             header: "Number Of Students Placed",
             content: "300 Bright and Astute minds who have managed to get placed successfully",
             stat: 300
         },
         {
-            imgSrc: {reactimage},
+            imgSrc: firstimage,
             header: "Number Of Students Placed",
             content: "300 Bright and Astute minds who have managed to get placed successfully",
             stat: 300
         },
         {
-            imgSrc: "images/Moneyincome-amico1.png",
+            imgSrc: firstimage,
             header: "Number Of Students Placed",
             content: "300 Bright and Astute minds who have managed to get placed successfully",
             stat: 300
@@ -26,7 +24,7 @@ export const Carousel1 = () => {
     return (<div className="w-full overflow-scroll bg-gray-100 h-[600px] my-4">
         <div className={`w-[1000px] py-12 flex h-full`}>
             {datas.map((data, i) => (<div key={i} className={`shadow-lg w-1/${datas.length} bg-white rounded-3xl flex flex-col justify-around items-center h-full mx-4 py-12 px-4`}>
-                <img className="object-contain h-1/2 bg-gray-100" src={data.imgSrc} />
+                <img className="object-contain h-1/2 bg-gray-100" src={data.imgSrc.src}/>
                 <p className="font-bold text-lg text-center"> {data.header} </p>
                 <p className="text-6xl z-[1] relative font-extrabold before:absolute before:top-6 before:-bottom-0 before:-left-2 before:-right-2 before:bg-primary-light before:z-[-1] before:rounded-tl-[9999px] before:rounded-br-[9999px]"> {data.stat} </p>
                 <p className="text-center"> {data.content} </p>
