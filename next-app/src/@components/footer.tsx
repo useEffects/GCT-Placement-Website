58fb78b854c26fbae240238f3b654e7ca9112ddf
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-11-10 12:39:58 +0530

    Footer edit

--- next-app/src/@components/footer.tsx
+++ next-app/src/@components/footer.tsx
@@ -50,23 +50,27 @@ export default function Footer () {
             ]
         }
     ]
-    return <div className="px-12">
-    <div className="w-full flex flex-col md:flex-row-reverse justify-around">
-        <div className="flex items-center justify-center w-full pb-12 col-2 col-span-2 md:w-1/3">
-            {datas.map((data, i) => <div key={i} className="flex flex-col justify-start items-center w-1/3 mx-4 h-full">
-                <p className="text-lg md:text-xl font-bold text-center my-4"> {data.header} </p>
-                <div className="flex flex-col items-start justify-start">
-                    {data.buttons.map((button, j) => <Link className="text-blue-500 underline md:text-lg my-1 md:my-2" key={j} href={button.link}>
-                        {button.label}
-                    </Link>)}
+    return <div className="px-12 bg-gray-100">
+    <div className="w-full flex flex-col-reverse md:flex-row-reverse justify-around">
+        <div className="flex flex-col sm:flex-row items-center justify-center w-full pb-12 col-2 col-span-2 md:w-2/3 p-4">
+            {datas.map((data, i) => <div key={i} className="flex flex-col justify-start items-center w-1/3 mt-4 h-full  ">
+                <p className="text-lg md:text-xl font-bold text-center text-black-700"> {data.header} </p>
+                <div className="flex flex-col items-start justify-start text-center mt-3 ">
+                    {data.buttons.map((button, j) => <div className="w-full my-2 p-2 hover:bg-gray-200">
+                        <Link className="text-black-500  my-2 hover:underline  p-2 w-100 " key={j} href={button.link}>
+                            {button.label}
+                        </Link>
+                    </div>
+                    )}
                 </div>
             </div>)}
         </div>
-        <div className="hidden md:block border-t-2 flex justify-between items-center md:border-t-0 border-gray-400 w-full md:w-1/3 mt-12 p-4 flex justify-evenly items-center">
-            <img src={Gctlogo.src} className="w-14 md:20 h:24 object-contain"/>
-            <div className="">
-                <p className="md:text-lg text-right font-bold uppercase"> 2022 GCT.<br></br>  All rights <br className="md:hidden"/> reserved. </p>
-                <p className="my-4 text-gray-600 hidden md:inline"> Training & Placement Cell </p>
+        <div className="md:block border-t-2 flex md:border-t-0 border-gray-400 w-full md:w-1/3 p-4 justify-evenly align-center flex-col">
+            <img src={Gctlogo.src} className="w-12 md:20 h:24 object-contain"/>
+            <div className="mt-2 m-auto">
+                <p className="my-4 md:text-lg text-left font-bold uppercase"> Training & Placement Cell </p>
+                <p className="my-4 text-gray-600">A carefully curated space for sharing all placement related activities in order to motivate and facilitate students to train meticulously and achieve great results.</p>
+                <p className="my-4 text-gray-600 text-left font-bold uppercase"> 2022 GCT.<br></br>  All rights <br className="md:hidden"/> reserved. </p>
             </div>
         </div>
     </div>
