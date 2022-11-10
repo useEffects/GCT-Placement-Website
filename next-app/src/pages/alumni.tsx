2f1818f645a79c605f2578dd7fd00f7a4acc659d
SaranPradeepJ <saranpradeep2003@gmail.com>
2022-11-11 01:19:05 +0530

    Alumni dine

---
+++ next-app/src/pages/alumni.tsx
@@ -0,0 +1,47 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import { alumniDatas } from '../page-datas/alumni'
+import alumniImages from '../images/alumniimg/1.jpg'
+function alumni() {
+  return (
+    <Container>
+        <div className='my-5 p-5'>
+            <div className="w-2/3">
+                <p className='uppercase text-2xl font-bold'>Notable Alumni</p>
+                <p className='text-sm mt-2'>In the past 75 years, the college has produced many stalwarts whose contribution at national and
+    international level has been significant. The achievements of the illustrious Alumni are a testament to the
+    quality of the institution and is amongst the best in the country. Here are the few past students who gets
+    featured in this otherwise exhaustive list.</p>
+            </div>
+            <div className="w-full mt-3">
+                {alumniDatas.map(alumniCate=>{
+                    return(
+                        <div className="w-full flex flex-wrap justify-between">
+                            <div className='w-1/3 flex justify-center items-center p-3 mt-2'>
+                                <p className='text-3xl text-gray-500 font-bold text-center'>{alumniCate.category}</p>
+                            </div>
+                            {alumniCate.people.map(people=>{
+                                return(
+                                    <div className='w-1/3 flex align-center p-3 bg-gray-100 rounded-3xl mt-2'>
+                                        <div className='w-1/3'>
+                                            <img src={alumniImages.src} alt="" />
+                                        </div>
+                                        <div className='flex flex-col flex-wrap w-2/3 px-2'>
+                                            <p className='text-md font-bold text-left mt-2'>{people.Name}</p>
+                                            <p className='text-sm text-gray-700 text-left mt-2'>{people.position}</p>
+                                            <p className='text-sm text-gray-700 text-left mt-2'>{people.company}</p>
+                                        </div>
+                                        
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </Container>
+  )
+}
+
+export default alumni
